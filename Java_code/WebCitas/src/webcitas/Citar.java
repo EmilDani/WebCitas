f56cde80c9a34d77f1cb23dcef7a101b860f1980
0290708 <0290708@dit151.lab.it.uc3m.es>
2015-11-26 13:18:49 +0100

    .

---
+++ Java_code/WebCitas/src/webcitas/Citar.java
@@ -0,0 +1,57 @@
+package webcitas;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.sql.SQLException;
+import javax.naming.NamingException;
+import java.util.List;
+
+public class Citar extends HttpServlet {
+	
+//	public void init() throws ServletException {
+//		
+//	}
+//	
+//	public void destroy() {
+//		
+//	}
+	
+//	public boolean isLogged (HttpSession session, DBManager manager)
+//			throws SQLException{
+//		boolean logged = false;
+//		User user = (User) session.getAttribute("user");
+//		if (user != null){
+//			// NO SÉ SI El if A CONTINUACIÓN ES REALMENTE ÚTIL
+//			if (manager.searchUser(user.getNickuser()) != null)
+//				logged = true;
+//		}
+//		return logged;
+//	}
+	
+//	public void doGet(HttpServletRequest request, HttpServletResponse response)
+//			throws IOException, ServletException {
+//		
+//		HttpSession session = request.getSession();
+//		try (DBManager manager = new DBManager()){
+//			if (isLogged (session, manager)){
+//				
+//				DinnerDate date= new DinnerDate();
+//				date = (DinnerDate) session.getAttribute("citas");
+//			// Variable citas a crear en el JSP de citas (Servlet)
+//				
+//				manager.setDate(date);
+//			}
+//
+//		} catch (SQLException | NamingException e) {
+//			
+//			PrintWriter out = response.getWriter();
+//		    out.println("ERROR");
+//		    e.printStackTrace();
+//			
+//		}
+//		
+//	}
+
+}
---
+++ Java_code/WebCitas/src/webcitas/DinnerDate.java
@@ -0,0 +1,51 @@
+package webcitas;
+
+import java.sql.Date;
+
+public class DinnerDate {
+	
+	private meetState state;
+	private User proposer;
+	private User receiver;
+	private Date proposal_sello;
+	private Date response_sello;
+	private int id_cita;
+	
+	public meetState getState() {
+		return state;
+	}
+	public void setState(meetState state) {
+		this.state = state;
+	}
+	public User getProposer() {
+		return proposer;
+	}
+	public void setProposer(User proposer) {
+		this.proposer = proposer;
+	}
+	public User getReceiver() {
+		return receiver;
+	}
+	public void setReceiver(User receiver) {
+		this.receiver = receiver;
+	}
+	public Date getProposal_sello() {
+		return proposal_sello;
+	}
+	public void setProposal_sello(Date proposal_sello) {
+		this.proposal_sello = proposal_sello;
+	}
+	public Date getResponse_sello() {
+		return response_sello;
+	}
+	public void setResponse_sello(Date response_sello) {
+		this.response_sello = response_sello;
+	}
+	public int getId() {
+		return id_cita;
+	}
+	public void setId(int id) {
+		this.id_cita = id_cita;
+	}
+
+}
